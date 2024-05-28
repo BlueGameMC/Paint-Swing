d0f325b997869a2a811c4574935a484f8d56dae9
BlueGameMC <101770837+BlueGameMC@users.noreply.github.com>
2024-05-28 21:24:57 +0000

    Thats probably this done

--- main.js
+++ main.js
@@ -6,30 +6,29 @@ const ctx = canvas.getContext("2d");
 
 var canvasSize = 2000;
 
-var steps = 1;
-var loops = 1;
-
-var canvasPos = [Math.random()*canvasSize/2-canvasSize/4,Math.random()*canvasSize/2-canvasSize/4];
+var steps = 2;
+var loops = 10;
+var canvasPos = [Math.random()*canvasSize-canvasSize/2,Math.random()*canvasSize-canvasSize/2];
 //var canvasPos = [0,0];
-var canvasVel = [Math.random()*80-40,Math.random()*80-40];
-var canvasWeight = Math.random()*100+50
+var canvasVel = [Math.random()*160-80,Math.random()*160-80];
+var canvasWeight = Math.random()*250+10
 
-var bucketPos = [Math.random()*canvasSize/2-canvasSize/4,Math.random()*canvasSize/2-canvasSize/4];
+var bucketPos = [Math.random()*canvasSize/.5-canvasSize/2,Math.random()*canvasSize/.5-canvasSize/2];
 var oldBucketPos = bucketPos.slice();
 oldBucketPos[0] -= canvasPos[0];
 oldBucketPos[1] -= canvasPos[1];
-var bucketVel = [Math.random()*80-40,Math.random()*80-40];
-var bucketWeight = Math.random()*100+50
+var bucketVel = [Math.random()*160-40,Math.random()*160-80];
+var bucketWeight = Math.random()*150+10
 
 ctx.lineWidth = Math.random()*5+2;
-let bgColor = [Math.random()*255,Math.random()*255,Math.random()*255];
+let bgColor = [Math.random()*205,Math.random()*205,Math.random()*205];
 let lineColor = [Math.random()*255,Math.random()*255,Math.random()*255];
 let escapeLoop = false;
 while(!escapeLoop)
 {
     if(Math.abs(rgb2hsv(bgColor[0],bgColor[1],bgColor[2])[0]-rgb2hsv(lineColor[0],lineColor[1],lineColor[2])[0]) <= 250)
     {
-        bgColor = [Math.random()*255,Math.random()*255,Math.random()*255];
+        bgColor = [Math.random()*205,Math.random()*205,Math.random()*205];
         lineColor = [Math.random()*255,Math.random()*255,Math.random()*255];
     }
     else
@@ -50,24 +49,25 @@ function draw()
 {
     for (let i = 0; i < loops; i++) {
         
+        ctx.lineWidth = Math.random()*4+2;
         
         //ctx.clearRect(0,0,400,400)
         bucketPos[0] += bucketVel[0]/steps;
         bucketPos[1] += bucketVel[1]/steps;
         bucketVel[0] += (0 - bucketPos[0])/bucketWeight/steps;
         bucketVel[1] += (0 - bucketPos[1])/bucketWeight/steps;
-        bucketVel[0] -= (bucketVel[0] * 0.0001)/steps;
-        bucketVel[1] -= (bucketVel[1] * 0.0001)/steps;
+        //bucketVel[0] -= (bucketVel[0] * 0.001)/steps;
+        //bucketVel[1] -= (bucketVel[1] * 0.001)/steps;
 
         canvasPos[0]+=canvasVel[0]/steps;
         canvasPos[1]+=canvasVel[1]/steps;
         canvasVel[0] += (0 - canvasPos[0])/canvasWeight/steps;
         canvasVel[1] += (0 - canvasPos[1])/canvasWeight/steps;
-        canvasVel[0] -= (canvasVel[0] * 0.0001)/steps;
-        canvasVel[1] -= (canvasVel[1] * 0.0001)/steps;
+        //canvasVel[0] -= (canvasVel[0] * 0.001)/steps;
+        //canvasVel[1] -= (canvasVel[1] * 0.001)/steps;
 
-        canvas.style.left = canvasPos[0] + canvasSize/2 +"px";
-        canvas.style.top = canvasPos[1] + canvasSize/2 +"px";
+        //canvas.style.left = canvasPos[0] + canvasSize/2 +"px";
+        //canvas.style.top = canvasPos[1] + canvasSize/2 +"px";
 
         bucket.style.left = bucketPos[0] + canvasSize - 10 + "px";
         bucket.style.top = bucketPos[1] + canvasSize - 10 +"px";
